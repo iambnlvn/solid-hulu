d974b7575d92ee10b03007270f4d2cd23653e7a3
iambnlvn <iambnlvn@gmail.com>
2025-02-13 17:52:39 -0600

    fix: wrap Router in MetaProvider for enhanced SEO support

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Route, Router } from "@solidjs/router";
 import type { Component } from "solid-js";
 import Home from "./pages/Home";
 import Trending from "./pages/Trending";
+import { MetaProvider } from "@solidjs/meta";
 
 const App: Component = () => {
   return (
-    <Router>
-      <Route path="/" component={Home} />
-      <Route path="/trending" component={Trending} />
-    </Router>
+    <MetaProvider>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/trending" component={Trending} />
+      </Router>
+    </MetaProvider>
   );
 };
 export default App;
