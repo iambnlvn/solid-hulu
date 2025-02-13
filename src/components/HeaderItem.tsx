7201bd97a5f0fbe50016441858edce85ee6540f4
iambnlvn <iambnlvn@gmail.com>
2025-02-13 14:15:24 -0600

    feat: enhance Home page with movie fetching logic and loading state; update HeaderItem styles

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -8,7 +8,7 @@ interface HeaderItemProps {
 const HeaderItem: Component<HeaderItemProps> = (props) => {
   return (
     <div class="group flex w-12 cursor-pointer flex-col items-center hover:text-white sm:w-20">
-      <props.Icon class="mb-1 h-8 group-hover:animate-bounce" />
+      <props.Icon class="h-8 group-hover:animate-bounce" />
       <p class="tracking-wider opacity-0 group-hover:opacity-100">
         {props.title}
       </p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,32 @@
 import { useSearchParams } from "@solidjs/router";
+import { createResource } from "solid-js";
+import { Response } from "../../types/requests";
+import Container from "../components/Container";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import { requests, trending } from "../constants/data";
+
+type Genre = keyof typeof requests;
 
 export default function Home() {
-  const [searchParams] = useSearchParams();
+  const fetchMovies = async (genre: string): Promise<Response> => {
+    const url = `https://api.themoviedb.org/3${requests[genre as Genre]?.url || trending.url}`;
 
+    // eslint-disable-next-line no-undef
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.results;
+  };
+  const [searchParams] = useSearchParams();
+  const [results] = createResource<Response, string, Genre>(
+    () => (searchParams.genre as Genre) || "trending",
+    fetchMovies,
+  );
   return (
     <div>
       <Header />
       <Navbar />
-      <h1>{searchParams.genre}</h1>
+      {results.loading ? <p>Loading...</p> : <Container results={results()} />}
     </div>
   );
 }
